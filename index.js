ebc1c9db676783347e00690a4fd04c03e4bfc439
Todd Carlson <todd.carlson3030@me.com>
2019-04-10 23:03:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,4 +28,8 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = function()
+var theFunk = function() {
+  return function() {
+    return "FUNKY!"
+  }
+}
